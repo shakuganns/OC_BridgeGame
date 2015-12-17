82019f6c02304ef32bdd68007922501cccac2fa5
shakuganns <z18679760733@gmail.com>
2015-12-17 14:20:19 +0800

    Initial Commit

---
+++ littleboy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  littleboy
+//
+//  Created by 钟圣麟 on 15/12/17.
+//  Copyright © 2015年 钟圣麟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ littleboyTests/littleboyTests.m
@@ -0,0 +1,39 @@
+//
+//  littleboyTests.m
+//  littleboyTests
+//
+//  Created by 钟圣麟 on 15/12/17.
+//  Copyright © 2015年 钟圣麟. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface littleboyTests : XCTestCase
+
+@end
+
+@implementation littleboyTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
